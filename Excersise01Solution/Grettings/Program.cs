89fc02aeea9c8efe060e78ab7356d41992fc60c1
rezanina.emil <rezanina.emil@gmail.com>
2019-02-18 19:20:34 +0100

    Complete first exercise.

--- Excersise01Solution/Grettings/Program.cs
+++ Excersise01Solution/Grettings/Program.cs
@@ -8,8 +8,45 @@ namespace Grettings
 {
     class Program
     {
+        const uint DEFAULT_COUNT = 100;
+
+        static uint GetCount(string[] args)
+        {
+            if (args.Length == 0) return DEFAULT_COUNT;
+            if (uint.TryParse(args[0], out uint temp))
+                return temp;
+            return DEFAULT_COUNT;
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Hello world!");
+            Console.WriteLine("Hello world again!");
+            uint count = GetCount(args);
+            //if (count != 0)
+            //{
+            //    string grettings = "Hello world ";
+            //    grettings += "again";
+            //    for (uint i = 1; i < count; i++)
+            //    {
+            //        grettings += " again";
+            //    }
+            //    grettings += "!";
+            //    Console.WriteLine(grettings);
+            //}
+
+            if (count != 0)
+            {
+                StringBuilder grettingsBuilder = new StringBuilder();
+                grettingsBuilder.Append("Hello world again");
+                for (uint i = 1; i < count; i++)
+                {
+                    grettingsBuilder.Append(" again");
+                }
+                grettingsBuilder.Append("!");
+                Console.WriteLine(grettingsBuilder.ToString());
+            }
+
         }
     }
 }
