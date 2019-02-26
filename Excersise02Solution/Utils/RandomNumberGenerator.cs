e5c7ad84d320ce79dd99d717b1dc7ea054bc7c72
rezanina.emil <rezanina.emil@gmail.com>
2019-02-26 16:46:52 +0100

    Add Excersise02 work.

---
+++ Excersise02Solution/Utils/RandomNumberGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Utils
+{
+    /// <summary>
+    /// Random number generator with <-10, 10> range.
+    /// </summary>
+    public static class RandomNumberGenerator
+    {
+        const int MIN_VALUE = -10;
+        const int MAX_VALUE = 10;
+        static Random random = new Random();
+
+        /// <summary>
+        /// Print class name without namespaces.
+        /// </summary>
+        public static void GetClassName()
+        {
+            Console.WriteLine(nameof(RandomNumberGenerator));
+        }
+
+        /// <summary>
+        /// Get next random number between [-10, 10]
+        /// </summary>
+        /// <returns>random number</returns>
+        public static int Next()
+        {
+            return random.Next(MIN_VALUE, MAX_VALUE);
+        }
+
+    }
+}
