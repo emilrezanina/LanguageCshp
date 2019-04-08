6a753452a4e5cb0d4c328f224fb45d949707df0f
rezanina.emil <rezanina.emil@gmail.com>
2019-04-08 18:59:13 +0200

    Add sample of DirectoryAnalyzer.

---
+++ Excersise08Solution/DirectoryAnalyzer/DirAnalyzer.cs
@@ -0,0 +1,75 @@
+﻿using NDesk.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DirectoryAnalyzer
+{
+    public class DirAnalyzer
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = typeof(DirAnalyzer).Name;
+            DirectoryAnalyzer analyzer = new DirectoryAnalyzer() { PrintInfos = false, DirectoryName = "." };
+            bool showHelp = false;
+            string outputName = null;
+
+            var options = new OptionSet
+            {
+                { "n|name=", "the name of directory.", name => analyzer.DirectoryName = name },
+                { "a|print-all", "print all information about foulders and files", val => analyzer.PrintInfos = true },
+                { "o|output=", "the name of output file.", output => outputName = output },
+                { "h|help", "show this message and exit", val => showHelp = val != null }
+            };
+
+            IList<string> extra = new List<string>();
+            try
+            {
+                extra = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine($"{Console.Title}: {e.Message}");
+                Console.WriteLine($"Try '{Console.Title} --help' for more information.");
+                return;
+            }
+
+            if (showHelp)
+            {
+                ShowHelp(options);
+                return;
+            }
+            InitAnalyzerStream(analyzer, outputName);
+            analyzer.Analyze();
+            foreach (var warning in analyzer.Warnings)
+            {
+                Console.WriteLine(warning);
+            }
+
+        }
+
+        private static void InitAnalyzerStream(DirectoryAnalyzer analyzer, string outputName)
+        {
+            if (outputName == null)
+            {
+                analyzer.Output = Console.OpenStandardOutput();
+                analyzer.Output.Flush();
+            }
+            else
+            {
+                analyzer.Output = File.Create(outputName);
+            }
+        }
+
+        private static void ShowHelp(OptionSet optionSet)
+        {
+            Console.WriteLine($"Usage: {Console.Title} [OPTIONS]");
+            Console.WriteLine($"{Console.Title} is tool for directory analysis.");
+            Console.WriteLine("Default directory is current.");
+            Console.WriteLine("Default output is console.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            optionSet.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
---
+++ Excersise08Solution/DirectoryAnalyzer/DirectoryAnalyzer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DirectoryAnalyzer
+{
+    public class DirectoryAnalyzer
+    {
+        private StreamWriter writer = null;
+        private readonly IDictionary<string, int> fileExtensionCounter = new Dictionary<string, int>();
+
+        public bool PrintInfos { get; set; }
+        public string DirectoryName { get; set; }
+        public Stream Output { get; set; }
+        public IList<string> Warnings { get; } = new List<string>();
+
+        public void Analyze()
+        {
+            fileExtensionCounter.Clear();
+            try
+            {
+                using (writer = new StreamWriter(Output))
+                {
+                    ProcessDirectory(new DirectoryInfo(DirectoryName));
+                    PrintFilesCounts();
+                }
+            }
+            catch (Exception e)
+            {
+                Warnings.Add(e.Message);
+            }
+        }
+
+        private void ProcessDirectory(DirectoryInfo directory)
+        {
+            if (PrintInfos)
+                PrintDirectoryInfo(directory);
+            if (!directory.Exists)
+                return;
+            ProcessFiles(directory);
+            ProcessSubdirectories(directory);
+        }
+
+        private void PrintDirectoryInfo(DirectoryInfo directory)
+        {
+            writer.WriteLine($"Directory '{directory.FullName}' exist: {directory.Exists}");
+            if (!directory.Exists)
+                return;
+
+            writer.WriteLine($"Directory name: {directory.Name}");
+            writer.WriteLine($"Directory root: {directory.Root}");
+            writer.WriteLine($"Directory parent: {directory.Parent}");
+            writer.WriteLine($"Directory attributes: {directory.Attributes}");
+            writer.WriteLine($"Directory creation time: {directory.CreationTime}");
+            writer.WriteLine($"Directory last write time: {directory.LastWriteTime}");
+        }
+
+        private void ProcessSubdirectories(DirectoryInfo directory)
+        {
+            foreach (var subdirectory in directory.GetDirectories())
+            {
+                ProcessDirectory(subdirectory);
+            }
+        }
+
+        private void ProcessFiles(DirectoryInfo directory)
+        {
+            foreach (var file in directory.GetFiles())
+            {
+                ProcessFile(file);
+            }
+        }
+
+        private void ProcessFile(FileInfo file)
+        {
+            ProcessFileExtension(file);
+            if (PrintInfos)
+                PrintFileInfo(file);
+        }
+
+        private void ProcessFileExtension(FileInfo file)
+        {
+            if (fileExtensionCounter.ContainsKey(file.Extension))
+                fileExtensionCounter[file.Extension]++;
+            else
+                fileExtensionCounter.Add(file.Extension, 1);
+        }
+
+        private void PrintFileInfo(FileInfo file)
+        {
+            writer.WriteLine($"File name: {file.Name}");
+            writer.WriteLine($"File extension: {file.Extension}");
+            writer.WriteLine($"File is readonly: {file.IsReadOnly}");
+            writer.WriteLine($"File last access time: {file.LastAccessTime}");
+            writer.WriteLine($"File last write time: {file.LastWriteTime}");
+            writer.WriteLine($"File has size: {file.Length} bytes");
+        }
+
+        private void PrintFilesCounts()
+        {
+            writer.WriteLine();
+            writer.WriteLine("Files extensions counts:");
+            foreach (var item in fileExtensionCounter)
+            {
+                writer.WriteLine($"*{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
